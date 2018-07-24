4df1a16586054c5b75662acf0fdf7db3c8780338
Marcin Jarząbek <marcin.jarzabek.pl@gmail.com>
2018-07-24 16:49:02 +0200

    Add class User
    Added class User, responsible for sth.

---
+++ src/main/java/pl/net/bluesoft/User.java
@@ -0,0 +1,19 @@
+package pl.net.bluesoft;
+
+public class User {
+    private String username;
+    private int age;
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
